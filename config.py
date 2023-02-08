128ff23130984ee018769084984352b15022311e
Graham-Broughton <broughton.graham@gmail.com>
2023-02-08 13:00:06 -0800

    multiclass almost works

--- config.py
+++ config.py
@@ -6,5 +6,5 @@ class CFG:
     URL: str = "https://drugcheckingbc.ca/results/"
     URL2: str = "https://drugcheckingbc.ca/dashboard/"
     WINDOW_SIZE: tuple = field(default_factory=lambda: (1280, 680))
-    CHROME_OPTIONS: list = field(default_factory=lambda: ["--headless", "--disable-gpu"])
+    CHROME_OPTS: list = field(default_factory=lambda: ["--disable-gpu",]) # "--headless", 
     
