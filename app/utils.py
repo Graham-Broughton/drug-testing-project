9047a920bf975c117c8a58e8a72a8adf8aca12a5
Graham-Broughton <broughton.graham@gmail.com>
2023-02-28 00:02:19 -0800

    changed opioid present function

--- app/utils.py
+++ app/utils.py
@@ -7,19 +7,10 @@ def opioid_present(col):
     """
     This function uses a list of regex strings to find if any opioids are present in a given column (ftir components).
     """
-    opioid_list = ['(despropionyl\s)?(para-fluoro)?(carf|f)entan(i|y)l( base)?', 
-                   'heroin',
-                   '(hydro|oxy).*one', 
-                   'opium', 
-                   'w-1(8|9)', 
-                   '6-mam', 
-                   '.*tazene', 
-                   '(code|morph)ine',
-                   '(furanyl\s)?uf-17']
-    score = sum(
-        1 if re.search(item, col, re.IGNORECASE) else 0 for item in opioid_list
-    )
-    return 1 if score > 0 else 0
+    return 1 if re.search(
+    '(.*an(y|i)l|heroin|(code|morph|buprenorph)ine|(oxy|hydro)(cod|morph)one|.*tazene|w-1(8|9)|opium|(furanyl\s)?uf-17|6-mam)',
+    col,
+    re.IGNORECASE) else 0
 
 def ftir_benzo(col):
     if col == 'Escitalopram': 
